468d800262852997e20eb9511d9a54c97e97d0b6
Kushagra Bhardwaj <kush2012bhardwaj@gmail.com>
2025-08-03 17:38:30 +0530

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,7 +5,7 @@ const firebaseConfig = {
   "projectId": "artistry-ai-h9ioy",
   "appId": "1:997696835273:web:b165712a660ec65ae28718",
   "storageBucket": "artistry-ai-h9ioy.firebasestorage.app",
-  "apiKey": "AIzaSyCs9gOVURSgcznQndgs0BUdN40EaOf5YBk",
+  "apiKey": "GEMINI_API_KEY",
   "authDomain": "artistry-ai-h9ioy.firebaseapp.com",
   "measurementId": "",
   "messagingSenderId": "997696835273"
