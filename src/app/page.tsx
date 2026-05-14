2bd7639de6c40cb60d13751ef27382ad951d4e26
Kushagra Bhardwaj <kush2012bhardwaj@gmail.com>
2026-05-14 23:57:23 +0530

    feat(home): remove extra sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,7 @@
 import { HeroSection } from "@/components/home/HeroSection";
-import { TrustBar } from "@/components/home/TrustBar";
 import { HowItWorks } from "@/components/home/HowItWorks";
-import { StyleShowcase } from "@/components/home/StyleShowcase";
-import { BeforeAfterShowcase } from "@/components/home/BeforeAfterShowcase";
 import { FeatureGrid } from "@/components/home/FeatureGrid";
-import { ProductMatching } from "@/components/home/ProductMatching";
 import { TestimonialCarousel } from "@/components/home/TestimonialCarousel";
-import { InspirationGallery } from "@/components/home/InspirationGallery";
-import { StatsSection } from "@/components/home/StatsSection";
 import { CTASection } from "@/components/home/CTASection";
 import { PremiumFooter } from "@/components/home/PremiumFooter";
 
@@ -15,15 +9,9 @@ export default function Home() {
   return (
     <main className="min-h-screen">
       <HeroSection />
-      <TrustBar />
       <HowItWorks />
-      <StyleShowcase />
-      <BeforeAfterShowcase />
       <FeatureGrid />
-      <ProductMatching />
       <TestimonialCarousel />
-      <InspirationGallery />
-      <StatsSection />
       <CTASection />
       <PremiumFooter />
     </main>
